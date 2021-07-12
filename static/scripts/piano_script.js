932fc1be8ecda10395b134e401a9a3875f6a24ec
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-07-12 17:42:10 +0300

    creating piano player

--- static/scripts/piano_script.js
+++ static/scripts/piano_script.js
@@ -2,9 +2,17 @@ const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const recordButton = document.querySelector('.record-button');
+const playButton = document.querySelector('.play-button');
+const saveButton = document.querySelector('.save-button');
+
 const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
+
+const keyMap = [...keys].reduce((map, key) => {
+    map[key.dataset.note] = key;
+    return map;
+}, {});
 
 let recordingStartTime;
 let songNotes;
@@ -25,6 +33,8 @@ document.addEventListener('keydown', event => {
 });
 
 recordButton.addEventListener('click', toggleRecording);
+playButton.addEventListener('click', playSong);
+saveButton.addEventListener('click', saveSong);
 
 function toggleRecording(event) {
     recordButton.classList.toggle('active');
@@ -39,14 +49,23 @@ function isRecording() {
 function startRecording() {
     recordingStartTime = Date.now();
     songNotes = [];
+    playButton.classList.remove('show');
+    saveButton.classList.remove('show');
 }
 
 function stopRecording() {
     playSong();
+    playButton.classList.add('show');
+    saveButton.classList.add('show');
 }
 
 function playSong() {
-    console.log(songNotes);
+    if (songNotes.length === 0) return;
+    songNotes.forEach(note => {
+        setTimeout(() => {
+            playNote(keyMap[note.key])
+        }, note.startTime);
+    })
 }
 
 function playNote(key) {
@@ -65,4 +84,8 @@ function recordNote(note) {
         key: note,
         startTime: Date.now() - recordingStartTime
     })
+}
+
+function saveSong() {
+
 }
