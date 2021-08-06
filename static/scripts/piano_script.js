e455cba25b7b89613284929729f644064bcbf4b4
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-08-06 19:08:15 +0300

    replacing the old notes/keys and adding new ones

--- static/scripts/piano_script.js
+++ static/scripts/piano_script.js
@@ -1,5 +1,5 @@
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j', 'l', ';']
 
 const currentSongInput = document.querySelector('#loaded-notes');
 const currentSong = currentSongInput ? JSON.parse(currentSongInput.value.replaceAll('\'', '"')) : null;
