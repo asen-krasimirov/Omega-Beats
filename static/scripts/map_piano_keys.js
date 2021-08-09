5c0603f51184ea1f67caa285f3eb5cdc42d2cf1a
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-08-09 13:59:43 +0300

    js bug fix

--- static/scripts/map_piano_keys.js
+++ static/scripts/map_piano_keys.js
@@ -1,6 +1,6 @@
 const keyTexts = [...document.querySelectorAll('.key-text')]
 
-document.querySelector('.map-button').addEventListener('click', () => {
+if (keyTexts) document.querySelector('.map-button').addEventListener('click', () => {
     for (const key of keyTexts) {
         key.classList.toggle('show');
     }
