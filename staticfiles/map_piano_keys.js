246a2509642dc7ceff0c9bbb5491c9704e3ca0c5
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-08-11 18:53:57 +0300

    deleting staticfiles from repo

--- staticfiles/map_piano_keys.js
+++
@@ -1,7 +0,0 @@
-const keyTexts = [...document.querySelectorAll('.key-text')]
-
-if (keyTexts) document.querySelector('.map-button').addEventListener('click', () => {
-    for (const key of keyTexts) {
-        key.classList.toggle('show');
-    }
-});
